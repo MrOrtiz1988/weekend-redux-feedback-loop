5af042e87981b8c2a451164b06cac328a3fd03e7
Luis Ortiz <l.ortizintec@gmail.com>
2023-05-06 12:45:03 -0400

    wrote function to send data to server

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,8 +1,21 @@
 import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 function Review () {
 
     const feedback = useSelector((store) => store.feedback);
+
+    const sendFeedback = () => {
+        axios({
+            method: 'POST',
+            url: '/feedback',
+            data: feedback
+        }).then( response => {
+            console.log(response);
+        }).catch(error => {
+            console.log('Problem with sendFeedback', error);
+        })
+    }
 
     return (
         <div>
@@ -11,7 +24,7 @@ function Review () {
             <h3>Understanding: {feedback.understanding}</h3>
             <h3>Support: {feedback.supported}</h3>
             <h3>Comments: {feedback.comments}</h3>
-            <button>SUBMIT</button>
+            <button onClick={sendFeedback}>SUBMIT</button>
         </div>
     )
 }
