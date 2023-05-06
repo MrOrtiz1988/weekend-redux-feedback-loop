5b710a92d8f5261750a3935263638a71373fd029
Luis Ortiz <l.ortizintec@gmail.com>
2023-05-05 20:55:00 -0400

    set up redux with boilerplate

--- src/index.js
+++ src/index.js
@@ -3,9 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
+// All the new Redux goodies:
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+const feedback = (state = {}, action) => {
+    return state;
+}
+
+const myStore = createStore(
+    combineReducers({
+        feedback
+    }),
+    applyMiddleware(
+        logger
+    )
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={myStore}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
