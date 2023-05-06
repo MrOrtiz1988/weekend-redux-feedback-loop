8b6c13e7c767c27a4cd891bc6eaeb8b045858292
Luis Ortiz <l.ortizintec@gmail.com>
2023-05-06 12:58:17 -0400

    post on server recieves data

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+const feedbackArray = [];
 
-
-
+router.post('/', (req, res) => {
+    const newFeedback = req.body;
+    console.log(newFeedback);
+    feedbackArray.push(newFeedback);
+    res.sendStatus(201);
+})
 
 
 module.exports = router;
