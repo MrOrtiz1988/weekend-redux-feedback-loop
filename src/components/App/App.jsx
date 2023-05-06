f66b635d1d3f8c7ae1e491c2d49e32416133e8eb
Luis Ortiz <l.ortizintec@gmail.com>
2023-05-06 11:54:26 -0400

    review component created and retrieves data from the redux store properly

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <Understanding />
       <Supported />
       <Comments />
-      {/* <Review /> */}
+      <Review />
     </div>
   );
 }
--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from 'react';
+import { useSelector } from 'react-redux';
 
 function Review () {
 
@@ -7,10 +7,11 @@ function Review () {
     return (
         <div>
             <h1>Review Your Feedback</h1>
-            <h3>Feelings: {feedback.feelings}</h3>
+            <h3>Feelings: {feedback.feeling}</h3>
             <h3>Understanding: {feedback.understanding}</h3>
             <h3>Support: {feedback.supported}</h3>
             <h3>Comments: {feedback.comments}</h3>
+            <button>SUBMIT</button>
         </div>
     )
 }
